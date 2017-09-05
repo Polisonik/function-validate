7b472028e5d91964faf5da3ff2a9944e7c43c5d0
Polisonik <allred@inbox.ru>
2017-09-05 19:13:00 +0300

    Исправление функций

--- validate.js
+++ validate.js
@@ -11,37 +11,25 @@
 
 	// проверка,если хештеги отсутствуют
   function isEmptyHashtag(hashtags) {
-    if (hashtags == '') { 
-      return true; 
-    } else return false;
+    return !!hashtags;
   }
   // Проверка первого симовла хештега на равенстов '#'
   function isFirstSymbolHashtag(event, hashtags) {
     for (var i = 0; i < hashtags.length; i++) {
-      if(history[i][0] != '#') {
-        event.preventDefault();
-        return true; (т.е. ошибка есть??)
-      } else return false;
+      return (hashtags[i][0] != '#');
     }
   }
   // Проверка на максимальное количество хештегов
   function isCountHashtegs(event, hashtags) {
     var hashtagsCount = 6;
-    
-		if (hashtags.length > hashtagsCount) {
-      event.preventDefault();
-      return true ;
-    } else return false;
+    return (hashtags.length > hashtagsCount)
   }
   // Проверка длины одного хештега
   function isMaxLength(event, hashtags) {
     var maxLength = 20;
 		
     for (var i = 0; i < hashtags.length; i++) {
-      if (hashtags[i].length > maxLength ) {
-        event.preventDefault();
-        return true;
-      } else return false;
+      return (hashtags[i].length > maxLength)
     }
   }
   // Проверка на одинаковые хештеги
@@ -49,10 +37,7 @@
     for(var i = 0; i < hashtags.length; i++) {
       var item = hashtags[i];
       for (var j = i+1; j < hashtags.length; j++) {
-        if (item === hashtags[j]) {
-          event.preventDefault();
-          return true;
-        } else return false;
+        return (item === hashtags[j])
       }
     }				
   }
