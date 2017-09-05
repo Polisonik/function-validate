ee458c880d25a42bfb359a8b4baac6565d3f6570
Polisonik <allred@inbox.ru>
2017-09-05 18:54:39 +0300

    Исправление функций

--- validate.js
+++ validate.js
@@ -3,46 +3,45 @@
   var inputHashtag = form.querySelector('.upload-form-hashtags');
   var hashtags = inputHashtag.value.split(' ');
   
+	isEmptyHashtag(hashtags);
   isFirstSymbol(event, hashtags);
   isCountHashtegs(event, mhashtags);
   isMaxLength(event, hashtags);
   isEqualHashtag(event, hashtags);
 
+	// проверка,если хештеги отсутствуют
+	function isEmptyHashtag(hashtags) {
+		if (hashtags == '') { 
+      return true; 
+    } else return false;
+	}
   // Проверка первого симовла хештега на равенстов '#'
-  function isFirstSymbol(event, hashtags) {
-    if (hashtags == '') { // проверка,если хештеги отсутствуют
-      return; 
-    }
+  function isFirstSymbolHashtag(event, hashtags) {
     for (var i = 0; i < hashtags.length; i++) {
       if(history[i][0] != '#') {
         event.preventDefault();
-        inputHashtag.style.borderColor = 'red';
-        return 'хэш-тег начинается с символа `#` (решётка) и состоит из одного слова';
-      }
+        return true; (т.е. ошибка есть??)
+      } else return false;
     }
   }
   // Проверка на максимальное количество хештегов
   function isCountHashtegs(event, hashtags) {
     var hashtagsCount = 6;
-    if (hashtags == '') { // проверка,если хештеги отсутствуют
-      return; 
-    }
-    if (hashtags.length > hashtagsCount) {
+    
+		if (hashtags.length > hashtagsCount) {
       event.preventDefault();
-      inputHashtag.style.borderColor = 'red';
-      return 'нельзя указать больше пяти хэш-тегов';
-    }
+      return true ;
+    } else return false;
   }
   // Проверка длины одного хештега
   function isMaxLength(event, hashtags) {
     var maxLength = 20;
-
+		
     for (var i = 0; i < hashtags.length; i++) {
       if (hashtags[i].length > maxLength ) {
         event.preventDefault();
-        inputHashtag.style.borderColor = "red";
-        return 'максимальная длина одного хэш-тега 20 символов';
-      }  
+        return true;
+      } else return false;
     }
   }
   // Проверка на одинаковые хештеги
@@ -52,9 +51,8 @@
       for (var j = i+1; j < hashtags.length; j++) {
         if (item === hashtags[j]) {
           event.preventDefault();
-          inputHashtag.style.borderColor = "red";
-          return 'один и тот же хэш-тег не может быть использован дважды';
-        }
+          return true;
+        } else return false;
       }
     }				
   }
